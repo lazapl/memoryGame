631a3a7d5bd1e3285babcf5e716eed0feede1e17
Daniel Jagoda <danielJagoda@o2.pl>
2023-12-28 15:47:04 +0100

    memorygame

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const generateRandomArray = (length: number) => {
+  const numbers: number[] = [];
+  for (let i = 1; i <= length / 2; i++) {
+    numbers.push(i, i);
+  }
+  return numbers.sort(() => Math.random() - 0.5);
+};
+
+const shuffleArray = (array: number[]) => {
+  return array.sort(() => Math.random() - 0.5);
+};
+
+const MemoGame: React.FC = () => {
+  const [cards, setCards] = useState<number[]>([]);
+  const [flippedIndices, setFlippedIndices] = useState<number[]>([]);
+  const [matchedPairs, setMatchedPairs] = useState<number[]>([]);
+
+  useEffect(() => {
+    setCards(shuffleArray(generateRandomArray(12)));
+  }, []);
+
+  const handleCardClick = (index: number) => {
+    if (flippedIndices.length === 2) {
+      return;
+    }
+
+    setFlippedIndices([...flippedIndices, index]);
+
+    if (flippedIndices.length === 1 && cards[flippedIndices[0]] === cards[index]) {
+      setMatchedPairs([...matchedPairs, cards[flippedIndices[0]]]);
+      setFlippedIndices([]);
+    }
+
+    if (flippedIndices.length === 1) {
+      setTimeout(() => {
+        setFlippedIndices([]);
+      }, 1000);
+    }
+  };
+
+  const renderCard = (value: number, index: number) => {
+    const isFlipped = flippedIndices.includes(index) || matchedPairs.includes(value);
+
+    return (
+      <div
+        key={index}
+        className={`card ${isFlipped ? 'flipped' : ''}`}
+        onClick={() => handleCardClick(index)}
+      >
+        {isFlipped ? value : '?'}
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory Game</h1>
+      <div className="card-container">
+        {cards.map((value, index) => renderCard(value, index))}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default MemoGame;
